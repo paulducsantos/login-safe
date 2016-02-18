e958955ced9318fcca5e965cb6b55ee30ce54668
Paul Santos <paulmichael.a.santos@gmail.com>
2016-02-17 21:15:31 -0500

    login works with validation and all

--- server.js
+++ server.js
@@ -1,89 +1,93 @@
 var express           = require('express');
 var expressHandlebars = require('express-handlebars');
 var bodyParser        = require('body-parser');
-var session           = require('express-session')
+var session           = require('express-session');
 var Sequelize         = require('sequelize');
+var sha256               = require('sha256');
 var app               = express();
 
 const PORT = process.env.PORT || 8080;
 
-var sequelize = new Sequelize('safe_entry', 'root', 'password');
+var sequelize = new Sequelize('User_db', 'root', 'password');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.engine('handlebars', expressHandlebars({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 var User = sequelize.define('User', {
-  name: {
+  username: {
     type: Sequelize.STRING,
     validate: {
-      is: ["^[a-z]+$",'i'],
-      notEmpty: true
+      len: [1,30]
     }
   },
-  phone_number: {
-    type: Sequelize.INTEGER,
-    validate: {
-      isNumeric: true
-    }
-  },
-  message: {
-    type: Sequelize.TEXT,
-    validate: {
-      len: [5,500]
-    }
+  password: {
+    type: Sequelize.STRING,
   }
 });
 
-// app.use(session({
-//   secret: 'elm i 8lsdjfklsjflkjsdfjsd',
-//   cookie: {
-//     maxAge: 1000 * 60 * 60 * 24 * 14
-//   },
-//   saveUninitialized: true,
-//   resave: false
-// }));
+app.use(session({
+  secret: 'have a gr8 day you are pr0',
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 14
+  },
+  saveUninitialized: true,
+  resave: false
+}));
 
 app.get('/', function(req, res) {
-  res.render('index');
+  res.render('register');
 });
 
-// app.get('/login', function(req, res) {
-//   res.render('login');
-// });
+app.get('/login', function(req, res) {
+  res.render('login');
+});
 
-app.post('/newperson', function(req, res) {
-  User.create(req.body).then(function(user) {
-    res.redirect('/');
+app.post('/register', function(req, res) {
+  var username = req.body.username;
+  if(req.body.password.length > 7) {
+    var password = sha256('porkchopsandwiches' + req.body.password);
+    User.create({username: username, password: password}).then(function(user) {
+      req.session.authenticated = user;
+      res.redirect('/');
+    }).catch(function(err) {
+      console.log(err);
+      res.redirect('/?msg=' + err.message);
+    });
+  } else {
+    res.render('fail');
+  }
+
+  
+});
+
+app.post('/login', function(req, res) {
+  var username = req.body.username;
+  var password = sha256('porkchopsandwiches' + req.body.password);
+
+  User.findOne({
+    where: {
+      username: username,
+      password: password
+    }
+  }).then(function(user) {
+    if(user) {
+      req.session.authenticated = user;
+      res.redirect('/?msg=login successful');
+    } else {
+      res.redirect('/?msg=You failed at life');
+    }
   }).catch(function(err) {
-    console.log(err);
-    res.redirect('/fail');
+    throw err;
   });
 });
 
-// app.post('/login', function(req, res) {
-//   var email = req.body.email;
-//   var password = req.body.password;
-
-//   Person.findOne({
-//     where: {
-//       email: email,
-//       password: password
-//     }
-//   }).then(function(user) {
-//     if(user) {
-//       req.session.authenticated = user;
-//       res.redirect('/success');
-//     } else {
-//       res.redirect('/?msg=You failed at life');
-//     }
-//   }).catch(function(err) {
-//     throw err;
-//   });
-// });
-
-app.get('/fail', function(req, res) {
-  res.render('fail');  
+app.get('/success', function(req, res) {
+  if(req.session.authenticated) {
+    res.render('success');
+  } else {
+    res.render('fail');
+  }
 });
 
 sequelize.sync().then(function() {
